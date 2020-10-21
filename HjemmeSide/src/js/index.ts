3d15162de68d7a53251707a1be9c0947d7514cbd
LeSubmarine <henrik@osterby.dk>
2020-10-21 12:33:06 +0200

    Update index.ts

--- HjemmeSide/src/js/index.ts
+++ HjemmeSide/src/js/index.ts
@@ -1,4 +1,3 @@
-import User from "./User";
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
