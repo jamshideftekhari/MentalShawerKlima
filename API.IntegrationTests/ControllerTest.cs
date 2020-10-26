15ac2697de4ba9576706040eb5f4c3f60618e698
Jakob Hammer <Jakob@thomasminde.dk>
2020-10-26 10:24:27 +0100

    Integration Test - initial commit

---
+++ API.IntegrationTests/ControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using ApiMentalShowerIndoor;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using ModelLib.Models;
+using Xunit;
+using ApiMentalShowerIndoor;
+
+namespace API.IntegrationTests
+{
+    public class ControllerTest
+    {
+        protected readonly string URI = "http://localhost:5000/api/IndoorClimate";
+        protected readonly HttpClient testClient;
+        protected ControllerTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(FanContext));
+                        services.AddDbContext<FanContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+            testClient = appFactory.CreateClient();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ API.IntegrationTests/IndoorClimateControllerIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ModelLib.Models;
+using Xunit;
+
+namespace API.IntegrationTests
+{
+    public class IndoorClimateControllerIntegrationTests : ControllerTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyData_ReturnsEmptyResponse()
+        {
+            //arrange
+
+
+            //act
+            var response = await testClient.GetAsync(URI);
+            
+            //assert
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+
+        }
+
+
+
+    }
+}
